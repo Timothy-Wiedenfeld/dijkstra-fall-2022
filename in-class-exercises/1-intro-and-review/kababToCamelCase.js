cadf102855820e1f64d5b17dae7d15dad6385db3
Timothy-Wiedenfeld <107075549+Timothy-Wiedenfeld@users.noreply.github.com>
2022-09-30 14:57:32 -0500

    Setup kabab to camel function

---
+++ in-class-exercises/1-intro-and-review/kababToCamelCase.js
@@ -0,0 +1,15 @@
+// Convert kebab case words into camel casing. The first word within the output should be capitalized only if it is the first letter of a word and dashes should be removed.
+
+// * change-this-word gets converted into changeThisWord
+// *day-month-year gets converted into dayMonthYear
+// *ALL-CAPs gets converted into allCaps
+
+
+
+function kababToCamel(kababString){
+    let camelString; //const. Const you have to declare becasuse it has to have a variable. You can not change const.
+    //let and var are the same thing but let is newer. let you can change later
+    //if char is "-" then dont use it AND capital following char test-string = testString
+    //otherwise lower case and copy to camelString
+    return (camelString);
+}
